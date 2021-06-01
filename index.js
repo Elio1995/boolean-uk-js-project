6c0842b841b9301eed26bed87553bcc78f1f2fff
Elio Mehmeti <mehmetielio95@gmail.com>
2021-06-01 23:10:20 +0100

    HardCoding

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const allFile = document.querySelector(`.all-file`)
-
+const leftMenuEl = document.querySelector(`.left-menu`)
+const mainSection = document.querySelector(`.main-section`)
+const rightMenuEl = document.querySelector(".right-menu")
 
 const state = {
-    drinks: []
+    drinks: [],
+    categories: []
 }
 
 
@@ -10,9 +13,9 @@ const state = {
 
 // HEADER SECTION 
 
-function renderHeaderSection() {
+const headerEl = document.querySelector(`.header-top`)
 
-    const headerEl = document.querySelector(`.header-top`)
+function renderHeaderSection() {
 
     const iconHeaderEl = document.createElement("img")
     iconHeaderEl.setAttribute("class", "logo")
@@ -30,9 +33,9 @@ function renderHeaderSection() {
 
 // LEFT MENU
 
+
 function renderLeftMenu() {
 
-    const leftMenuEl = document.querySelector(`.left-menu`)
 
     const h2LeftMenu = document.createElement("h2")
 
@@ -50,21 +53,21 @@ function renderLeftMenu() {
         alcoholicLiLeftEl1.innerText = "All"
         const alcoholicInputLeftEl1 = document.createElement("input")
         alcoholicInputLeftEl1.setAttribute("name", "alcoholic")
-        alcoholicInputLeftEl1.setAttribute("type", "checkbox")
+        alcoholicInputLeftEl1.setAttribute("type", "radio")
         alcoholicLiLeftEl1.append(alcoholicInputLeftEl1)
 
         const alcoholicLiLeftEl2 = document.createElement("li")
         alcoholicLiLeftEl2.innerText = "Alcoholic"
         const alcoholicInputLeftEl2 = document.createElement("input")
         alcoholicInputLeftEl2.setAttribute("name", "alcoholic")
-        alcoholicInputLeftEl2.setAttribute("type", "checkbox")
+        alcoholicInputLeftEl2.setAttribute("type", "radio")
         alcoholicLiLeftEl2.append(alcoholicInputLeftEl2)
 
         const alcoholicLiLeftEl3 = document.createElement("li")
         alcoholicLiLeftEl3.innerText = "Non-Alcoholic"
         const alcoholicInputLeftEl3 = document.createElement("input")
         alcoholicInputLeftEl3.setAttribute("name", "alcoholic")
-        alcoholicInputLeftEl3.setAttribute("type", "checkbox")
+        alcoholicInputLeftEl3.setAttribute("type", "radio")
         alcoholicLiLeftEl3.append(alcoholicInputLeftEl3)
 
 
@@ -94,8 +97,6 @@ function renderLeftMenu() {
 
 
 // MAIN SECTION
-
-const mainSection = document.querySelector(`.main-section`)
 
 function renderTopSection() {
     const topSection = document.createElement("section")
@@ -182,56 +183,127 @@ function renderBottomSection() {
 
 // RIGHT MENU
 
-{/* <div class="favourites-section">
-<h2>Favourites</h2>
-<ul class="favourites-list">
-  <li>
-    <h3>Old Fashioned</h3>
-  </li>
-  <li>
-    <h3>Old Fashioned</h3>
-  </li>
-</ul>
-</div>
 
-<div class="new-drink-form">
-<h2>Create Your Own Drink</h2>
-<label for="name">Name</label>
-<input type="text" name="name">
+// FAVOURITE SECTION
+function renderRightMenu() {
 
-<label for="categories">Category</label>
-<select type="select" name="categories" id="">
-  <option value disabled selected hidden>Select the Category</option>
-  <option value="1">Ordinary Drink</option>
-  <option value="2">Ordinary Drink</option>
-  <option value="3">Ordinary Drink</option>
-  <option value="4">Ordinary Drink</option>
-  <option value="5">Ordinary Drink</option>
-  <option value="6">Ordinary Drink</option>
-</select>
+    function favouritesList() {
 
-<ul class="alcoholic-form">
-  <label for="alcoholic">Alcoholic</label>
-  <li><input name="alcoholic" type="radio">All</li>
-  <li><input name="alcoholic" type="radio">Alcoholic</li>
-  <li><input name="alcoholic" type="radio">Non-Alcoholic</li>
-</ul>
+        const favouritesDivEl = document.createElement("div")
+        favouritesDivEl.setAttribute("class", "favourites-section")
 
-<label for="ingredients">Ingredients</label>
-<input type="text" name="ingredients">
+        const favouritesh2El = document.createElement("h2")
+        favouritesh2El.innerText = "Favourites"
 
-<label for="instructions">Instructions</label>
-<textarea name="instructions" rows="5" cols="20">
-</textarea>
+        const favouritesUlEl = document.createElement("ul")
+        favouritesUlEl.setAttribute("class", "favourites-list")
 
-<button>
-  Submit
-</button>
+        const favouritesLiEl = document.createElement("li")
+        favouritesUlEl.append(favouritesLiEl)
+        const favouritesh3El = document.createElement("h3")
+        favouritesh3El.innerText = ""
+        favouritesLiEl.append(favouritesh3El)
 
-</div>  */}
+        favouritesDivEl.append(favouritesh2El, favouritesUlEl)
+    }
+
+    function newDrinkForm() {
+
+        const newDrinkDivEl = document.createElement("div")
+        newDrinkDivEl.setAttribute("class", "new-drink-form")
+
+        const newDrinkh2EL = document.createElement("h2")
+        newDrinkh2EL.innerText = "Create Your Own Drink"
+
+        // NAME
+        const newDrinkNameLabelEl = document.createElement("label")
+        newDrinkNameLabelEl.setAttribute("for", "name")
+        newDrinkNameLabelEl.innerText = "Name"
+
+        const newDrinkNameInputEl = document.createElement("input")
+        newDrinkNameInputEl.setAttribute("type", "text")
+        newDrinkNameInputEl.setAttribute("name", "name")
+
+        // CATEGORY
+        const newDrinkCategoriesLabelEl = document.createElement("label")
+        newDrinkCategoriesLabelEl.setAttribute("for", "categories")
+        newDrinkCategoriesLabelEl.innerText = "Category"
+
+        const newDrinkCategoriesSelectEl = document.createElement("select")
+        // newDrinkCategoriesSelectEl.setAttribute("id", "")
+        newDrinkCategoriesSelectEl.setAttribute("type", "select")
+        newDrinkCategoriesSelectEl.setAttribute("name", "categories")
+
+        const newDrinkCategoriesOptionEl = document.createElement("option")
+        newDrinkCategoriesOptionEl.innerText = "Ordinary Drink"
+        newDrinkCategoriesOptionEl.setAttribute("value", "1?????")
+        newDrinkCategoriesSelectEl.append(newDrinkCategoriesOptionEl)
+
+        // ALCOHOLIC
+        const newDrinkAlcoholicFormUlEl = document.createElement("ul")
+        newDrinkAlcoholicFormUlEl.setAttribute("class", "alcoholic-form")
 
 
+        const newDrinkAlcoholicFormLabelEl = document.createElement("label")
+        newDrinkAlcoholicFormLabelEl.setAttribute("for", "alcoholic")
+        newDrinkAlcoholicFormLabelEl.innerText = "Alcoholic"
 
+
+        const newDrinkAlcoholicFormLiEl1 = document.createElement("li")
+        newDrinkAlcoholicFormLiEl1.innerText = "All"
+        const newDrinkAlcoholicFormInputEl1 = document.createElement("input")
+        newDrinkAlcoholicFormInputEl1.setAttribute("name", "alcoholic")
+        newDrinkAlcoholicFormInputEl1.setAttribute("type", "radio")
+        newDrinkAlcoholicFormLiEl1.append(newDrinkAlcoholicFormInputEl1)
+
+        const newDrinkAlcoholicFormLiEl2 = document.createElement("li")
+        newDrinkAlcoholicFormLiEl2.innerText = "All"
+        const newDrinkAlcoholicFormInputEl2 = document.createElement("input")
+        newDrinkAlcoholicFormInputEl2.setAttribute("name", "alcoholic")
+        newDrinkAlcoholicFormInputEl2.setAttribute("type", "radio")
+        newDrinkAlcoholicFormLiEl2.append(newDrinkAlcoholicFormInputEl2)
+
+        const newDrinkAlcoholicFormLiEl3 = document.createElement("li")
+        newDrinkAlcoholicFormLiEl3.innerText = "All"
+        const newDrinkAlcoholicFormInputEl3 = document.createElement("input")
+        newDrinkAlcoholicFormInputEl3.setAttribute("name", "alcoholic")
+        newDrinkAlcoholicFormInputEl3.setAttribute("type", "radio")
+        newDrinkAlcoholicFormLiEl3.append(newDrinkAlcoholicFormInputEl3)
+
+        newDrinkAlcoholicFormUlEl.append(newDrinkAlcoholicFormLabelEl, newDrinkAlcoholicFormLiEl1, newDrinkAlcoholicFormLiEl2, newDrinkAlcoholicFormLiEl3)
+
+
+        // INGREDIENTS
+        const newDrinksIngredientsLabelEl = document.createElement("label")
+        newDrinksIngredientsLabelEl.setAttribute("for", "ingredients")
+        newDrinksIngredientsLabelEl.innerText = "Ingredients"
+
+        const newDrinksIngredientsInputEl = document.createElement("input")
+        newDrinksIngredientsInputEl.setAttribute("type", "text")
+        newDrinksIngredientsInputEl.setAttribute("name", "ingredients")
+
+        // INSTRUCTIONS
+        const newDrinksInstructionsLabelEl = document.createElement("label")
+        newDrinksInstructionsLabelEl.setAttribute("for", "instructions")
+        newDrinksInstructionsLabelEl.innerText = "Instructions"
+
+        const newDrinksInstructionsTextAreaEl = document.createElement("textarea")
+        newDrinksInstructionsTextAreaEl.setAttribute("name", "instructions")
+        newDrinksInstructionsTextAreaEl.setAttribute("rows", "5")
+        newDrinksInstructionsTextAreaEl.setAttribute("cols", "20")
+
+        // SUBMIT BUTTON
+
+        const newDrinkSubmitButtonEl = document.createElement("button")
+        newDrinkSubmitButtonEl.innerText = "SUBMIT"
+
+        newDrinkDivEl.append(newDrinkh2EL, newDrinkNameLabelEl, newDrinkNameInputEl, newDrinkCategoriesLabelEl, newDrinkCategoriesSelectEl, newDrinkAlcoholicFormUlEl, newDrinksIngredientsLabelEl, newDrinksIngredientsLabelEl, newDrinksInstructionsLabelEl, newDrinksInstructionsTextAreaEl, newDrinkSubmitButton)
+    }
+
+    rightMenuEl.append(favouritesDivEl, newDrinkDivEl)
+}
+
+// CREATE ANOTHER JSON SERVER WITH THE CATEGORIES
 
 
 
@@ -255,6 +327,7 @@ function render() {
     renderLeftMenu()
     renderTopSection()
     renderBottomSection()
+    renderRightMenu()
 }
 
 getDataFromSever()
